a11135e391a363bdb5a77f8d88cc577aad3083fc
Michal9292 <michaljania0@gmail.com>
2026-06-21 13:20:00 +0200

    Dodano help

---
+++ skrypt.sh
@@ -0,0 +1,9 @@
+--help)
+
+    echo "Dostępne opcje:"
+    echo "--date  - wyświetla aktualną datę"
+    echo "--logs  - tworzy 100 plików log"
+    echo "--logs X - tworzy X plików log"
+    echo "--help  - wyświetla pomoc"
+
+    ;;
